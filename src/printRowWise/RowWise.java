dc4348bc43c2132cb3d5c352e418e1fa8d566ffe
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-03-24 00:14:38 +0530

    After defining all the logics required to print the 2D array / matrix as wave.

--- src/printRowWise/RowWise.java
+++ src/printRowWise/RowWise.java
@@ -1,5 +1,7 @@
 package printRowWise;
 
+import java.util.Scanner;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Abhinav Bhardwaj
@@ -8,4 +10,36 @@ package printRowWise;
  */
 
 public class RowWise {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int [][] input_matrix = new int[rows] [cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                input_matrix[row] [col] = sc.nextInt();
+            }
+        }
+
+
+        System.out.print("Start, ");
+        for (int row = 0; row < rows; row++) {
+            if(row % 2 == 0) {
+                int col = 0;    // initializing row with top
+                while (col < cols) {
+                    System.out.print(input_matrix[row] [col] + ", ");
+                    col++;
+                }
+            }
+            else {
+                int col = cols - 1;     // initializing row with bottom
+                while (col >= 0) {
+                    System.out.print(input_matrix[row] [col] + ", ");
+                    col--;
+                }
+            }
+        }
+        System.out.print("END");
+    }
 }
